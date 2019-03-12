892f725171d3075ae4a250e2dd1416361ce18063
Ripal Patel <ripal212007@gmail.com>
2019-03-11 19:20:22 -0500

    done

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collections
 {
@@ -6,7 +7,38 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var arrayOfObjects = new object[5];
+            var studentsWithBlackHair = new List<string> {"Nathan", "Austin", "Marty Mcfly"};//collection intializer beacause you are doing collection
+
+            var studentsByHairColor = new Dictionary<string, List<string>>
+            {
+                { "Black", studentsWithBlackHair }
+            };
+            studentsByHairColor.Add("Bald", new List<string> {"Martin"});
+            //studentsByHairColor.Add("Bald", new List<string> { "Adam" }); // it will trhow exception as you can't add multiple values to same key.
+
+            var theBlackHairedStudents= studentsByHairColor["Black"]; //gives value out of List. 
+
+            if (theBlackHairedStudents == studentsWithBlackHair)
+            {
+                Console.WriteLine("They are the same");
+            }
+
+            studentsByHairColor["Black"] = new List<string>();
+
+            studentsByHairColor["Black"].Add("Welly");
+            //kvp is key value pair. when you want to destructure use it(key, value) so its not wrapped around. and can access key and value. hairColor is key and students are value
+            foreach (var (hairColor, students) in studentsByHairColor)
+            {
+                Console.WriteLine($"The following students have {hairColor} hair");
+
+                //students.Add("new person");
+                foreach (var student in students)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
